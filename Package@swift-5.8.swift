cb1c97d40653b2a82a1a174266d09bc4c5686e26
Frizlab <fload@me.com>
2025-01-02 10:02:21 +0100

    Add package file for Swift 5.8+ w/ concurrency check

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+
+//let swiftSettings: [SwiftSetting] = []
+let swiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
+
+let package = Package(
+	name: "gha-logger",
+	platforms: [
+		.macOS(.v11),
+		.tvOS(.v14),
+		.iOS(.v14),
+		.watchOS(.v7),
+	],
+	products: [
+		.library(name: "GHALogger", targets: ["GHALogger"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-log.git",          from: "1.5.1"),
+		.package(url: "https://github.com/xcode-actions/clt-logger.git", from: "1.0.0-beta"),
+	],
+	targets: [
+		.target(name: "GHALogger", dependencies: [
+			.product(name: "CLTLogger", package: "clt-logger"),
+			.product(name: "Logging",   package: "swift-log"),
+		], path: "Sources", swiftSettings: swiftSettings),
+		.testTarget(name: "GHALoggerTests", dependencies: ["GHALogger"], swiftSettings: swiftSettings),
+	]
+)
